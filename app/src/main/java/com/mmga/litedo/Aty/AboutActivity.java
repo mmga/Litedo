260885bfec315eac1317c9d96e588e5f2454744e
mmga <1034752946@qq.com>
2015-10-29 13:36:23 +0800

    加了个about界面和LogUtil
    。

---
+++ app/src/main/java/com/mmga/litedo/Aty/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.mmga.litedo.Aty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mmga.litedo.R;
+
+/**
+ * Created by mmga on 2015/10/29.
+ */
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+}
---
+++ app/src/main/java/com/mmga/litedo/Util/DBUtil.java
@@ -0,0 +1,68 @@
+package com.mmga.litedo.Util;
+
+import com.mmga.litedo.db.Model.Memo;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+
+public class DBUtil{
+
+    /**
+     * 增加一个新的任务
+     * @param content
+     */
+    public static void addMemo(String content){
+        Memo memo = new Memo();
+        memo.setContent("" + content);
+        memo.setIsDone(0);
+        memo.save();
+    }
+
+    /**
+     * 将isDone标记为1，代表已完成的任务
+     * @param memoId
+     */
+    public static void deleteMemo(int memoId){
+        Memo memoToUpdate = new Memo();
+        memoToUpdate.setIsDone(1);
+        memoToUpdate.update(memoId);
+    }
+
+    /**
+     * 按照id倒序查找数据
+     * @return
+     */
+    public static List getAllMemo() {
+        return DataSupport.where("isDone = ?", "0")
+//                .order("id desc")
+                .find(Memo.class);
+    }
+
+    /**
+     * 获得应显示的memo数量
+     * @return
+     */
+    public static int getMemoNum() {
+        int num = DataSupport.where("isDone = ?", "0").count(Memo.class);
+        return num;
+    }
+
+    public static void exchangeMemo(int fromId, int toId) {
+
+        exchange(fromId, toId);
+    }
+
+    private static void exchange(int fromId, int toId) {
+        Memo fromMemo = DataSupport.find(Memo.class, fromId);
+        Memo toMemo = DataSupport.find(Memo.class, toId);
+        fromMemo.update(toId);
+        toMemo.update(fromId);
+    }
+
+
+
+
+
+}
