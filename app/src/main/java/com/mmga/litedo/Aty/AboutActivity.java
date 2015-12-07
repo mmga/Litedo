a6ca7ab22ec0150e1e990d6adf9198d1d72e1c13
mmga <1034752946@qq.com>
2015-12-07 23:50:51 +0800

    大体画了设置界面

--- app/src/main/java/com/mmga/litedo/Aty/AboutActivity.java
+++ app/src/main/java/com/mmga/litedo/Aty/AboutActivity.java
@@ -1,16 +1,28 @@
 package com.mmga.litedo.Aty;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mmga.litedo.R;
+import com.mmga.litedo.Util.StatusBarCompat;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends AppCompatActivity {
+
+    private AppBarLayout appBarLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_settings);
+        StatusBarCompat.compat(this, getResources().getColor(R.color.colorPrimaryDark));
+
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.setting);
 
     }
 }
